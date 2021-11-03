a8f397a7e2cf94779371019752593552706e090a
Gustavo2168 <79846041+Gustavo2168@users.noreply.github.com>
2021-11-03 19:13:43 -0300

    Add files via upload

---
+++ Comandos.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace G.I.V
+{
+    class Comandos
+    {
+        public int id, quantidade;
+        public string descricao;
+        public double valor;
+
+        Conexao obj = new Conexao();
+
+        public void Cadastrar()
+        {
+
+            string inserir = $@"insert into produtos values ('{id}', '{descricao}', '{valor}', '{quantidade}');";
+            obj.ExecutarComando(inserir);
+
+        }
+
+        public DataTable Listar()     //Requer: using System.Data;
+        {
+            string listar = $"SELECT * FROM produtos; ";
+            return obj.ExecutarConsulta(listar);
+        }
+        public void Excluir()
+        {
+            string deletar = $"DELETE FROM produtos WHERE id = '{id}'; ";
+            obj.ExecutarComando(deletar);
+        }
+
+        public void Alterar()
+        {
+            string alterar = $"UPDATE produtos SET descricao = '{descricao}', valor = '{valor}', quantidade = '{quantidade}' WHERE id = '{id}'";
+            obj.ExecutarComando(alterar);
+        }
+    }
+}
---
+++ Produtos.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace G.I.V
+{
+    public partial class Produtos : Form
+    {
+        public Produtos()
+        {
+            InitializeComponent();
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            Comandos obj = new Comandos();
+            dataGridView1.DataSource = obj.Listar();
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            Comandos obj = new Comandos();
+            obj.id = (int)Convert.ToInt64(txtId.Text);
+            obj.descricao = txtDescricao.Text;
+            obj.valor = Convert.ToDouble(txtValor.Text);
+            obj.quantidade = (int)Convert.ToInt64(txtQuantidade.Text);
+            obj.Cadastrar();
+            MessageBox.Show("Cadastro Efetuado!");
+            txtId.Clear();
+            txtDescricao.Clear();
+            txtValor.Clear();
+            txtQuantidade.Clear();
+            txtId.Focus();
+            dataGridView1.DataSource = obj.Listar();
+
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            Comandos obj = new Comandos();
+            obj.id = int.Parse(txtId.Text);
+            obj.Excluir();
+            MessageBox.Show("Excluído com Sucesso!");
+            txtId.Clear();
+            txtDescricao.Clear();
+            txtValor.Clear();
+            txtQuantidade.Clear();
+            txtId.Focus();
+            dataGridView1.DataSource = obj.Listar();
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            Comandos obj = new Comandos();
+            obj.id = (int)Convert.ToInt64(txtId.Text);
+            obj.descricao = txtDescricao.Text;
+            obj.valor = Convert.ToDouble(txtValor.Text);
+            obj.quantidade = (int)Convert.ToInt64(txtQuantidade.Text);
+            obj.Alterar();
+            MessageBox.Show("Alterado com Sucesso!");
+            txtId.Clear();
+            txtDescricao.Clear();
+            txtValor.Clear();
+            txtQuantidade.Clear();
+            txtId.Focus();
+            dataGridView1.DataSource = obj.Listar();
+        }
+    }
+}
